80c71d507dc3a5e1d6e909c2969331eb867a5f4d
rizkajuliani <73518592+rizkajuliani@users.noreply.github.com>
2020-11-11 17:54:10 +0700

    Initial commit

---
+++ system/resources/views/beranda.blade.php
@@ -0,0 +1,225 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+   
+   <nav class="navbar navbar-expand-lg navbar-light bg-warning fixed-top">
+    <div class="container">
+  <a class="navbar-brand font-weight-bold" href="#">TOKO FASHION WANITA</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto mr-4">
+       <li class="nav-item active">
+        <a class="nav-link" href="#">Hubungi Kami <span class="sr-only">(current)</span></a>
+      </li>
+       <li class="nav-item active">
+        <a class="nav-link" href="#">Bantuan <span class="sr-only">(current)</span></a>
+      </li>
+     
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+  </div>
+</nav>
+
+
+<div class="row mt-5 no-gutters">
+    <div class="col-md-2 bg-light">
+        <ul class="list-group list-group-flush list p-2 pt-4">
+  <li class="list-group-item bg-warning">KATEGORI PRODUK</li>
+  <li class="list-group-item">Fashion Pria</li>
+  <li class="list-group-item">Fashion Wanita</li>
+  <li class="list-group-item">Fashion Anak</li>
+</ul>
+    </div>
+    <div class="col-md-10">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/slide/slide1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/slide/slide2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/slide/slide3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+<h4 class="text-center font-weight-bold mb-5 mt-4">PRODUK TERBARU</h4>
+<h4 class="text-center font-weight-bold mb-4 mt-3">Fashion Wanita</h4>
+
+<div class="container">
+    <div class="row">
+        <div class=" col-md-3">
+    <div class="card">
+  <img src="img/produk/baju.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Setelan Baju dan Rok</h5>
+    <p class="card-text">Setelan baju dan rok yang nyaman dipakai</p>
+    <a href="#" class="btn btn-primary">Rp.158.998</a>
+  </div>
+</div>        
+        </div>  
+        <div class=" col-md-3">
+    <div class="card">
+  <img src="img/produk/rok prisket.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Rok Prisket</h5>
+    <p class="card-text">Rok yang banyak lipitan sebagai percantik</p>
+    <a href="#" class="btn btn-primary">Rp.58.889</a>
+  </div>
+</div>        
+        </div>  
+        <div class=" col-md-3">
+    <div class="card">
+  <img src="img/produk/jilbab pasmina.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Pashmina</h5>
+    <p class="card-text">Pashmina Ceruty yang super adem</p>
+    <a href="#" class="btn btn-primary">Rp.38.997</a>
+  </div>
+</div>        
+        </div>  
+        <div class=" col-md-3">
+    <div class="card">
+  <img src="img/produk/segi empat.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Bella Squere</h5>
+    <p class="card-text">Jilbab bella squere yang lembut</p>
+    <a href="#" class="btn btn-primary">Rp.18.999</a>
+  </div>
+</div>        
+        </div>  
+    </div>
+
+<h4 class="text-center font-weight-bold mb-5 mt-4">Fashion Pria</h4>
+
+<div class="container">
+    <div class="row">
+        <div class=" col-md-3">
+    <div class="card">
+  <img src="img/Pria/jaket.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Jaket Kekinian</h5>
+    <p class="card-text">Bahan tebal tidak panas</p>
+    <a href="#" class="btn btn-primary">Rp.798.999</a>
+  </div>
+</div>        
+        </div>  
+        <div class=" col-md-3">
+    <div class="card">
+  <img src="img/Pria/jam.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Jam Tangan</h5>
+    <p class="card-text">Jam Tangan Cowok yang super keren dan kece</p>
+    <a href="#" class="btn btn-primary">Rp.5.898.766</a>
+  </div>
+</div>        
+        </div>  
+        <div class=" col-md-3">
+    <div class="card">
+  <img src="img/Pria/topi.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Topi Cowok</h5>
+    <p class="card-text">Topi yang sangat kece badai</p>
+    <a href="#" class="btn btn-primary">Rp.2.999.888</a>
+  </div>
+</div>        
+        </div>  
+        <div class=" col-md-3">
+    <div class="card">
+  <img src="img/Pria/sepatu.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Sepatu Nike</h5>
+    <p class="card-text">Sepatu Nike Cowok yang keren kece parah</p>
+    <a href="#" class="btn btn-primary">Rp.10.778.999</a>
+  </div>
+</div>        
+        </div>  
+</div>
+<h4 class="text-center font-weight-bold mb-5 mt-4">Fashion Pria</h4>
+
+<div class="container">
+    <div class="row">
+        <div class=" col-md-3">
+    <div class="card">
+  <img src="img/Anak/penutup.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Bando</h5>
+    <p class="card-text">Bando anak yang super lucu</p>
+    <a href="#" class="btn btn-primary">Rp.15.777</a>
+  </div>
+</div>        
+        </div>  
+        <div class=" col-md-3">
+    <div class="card">
+  <img src="img/anak/baju.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Baju</h5>
+    <p class="card-text">Baju anak yang nyaman dan lembut</p>
+    <a href="#" class="btn btn-primary">Rp.35.888</a>
+  </div>
+</div>        
+        </div>  
+        <div class=" col-md-3">
+    <div class="card">
+  <img src="img/anak/celemek.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Celemek bayi</h5>
+    <p class="card-text">Celemek bayi yang kekinian</p>
+    <a href="#" class="btn btn-primary">Rp.23.555</a>
+  </div>
+</div>        
+        </div>  
+        <div class=" col-md-3">
+    <div class="card">
+  <img src="img/anak/sarungtangan.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Sarung Tangan</h5>
+    <p class="card-text">Sarung Tangan bayi yg lembut</p>
+    <a href="#" class="btn btn-primary">Rp.29.999</a>
+  </div>
+</div>        
+        </div>  
+   </div>
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
